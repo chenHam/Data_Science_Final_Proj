b5464521f1910bbbf15ecf7cc97e6a1faef75454
ChenHam <Chen@DESKTOP-8HEVHG2>
2018-03-22 18:34:35 +0200

    Initial commit

---
+++ helper_func.py
@@ -0,0 +1,16 @@
+
+# for Tickers On a Date
+def tod(date, tickers):
+    print()
+
+# for File for Ticker in range
+def f4t(start_date, end_date, ticker_name, file_name, file_format):
+    print()
+
+# Compare Ticks In range
+def ctir():
+    print()
+
+#
+def tick_compare(start_date, end_date,ticker):
+    print()
---
+++ tickers_data.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+
+
+# comment
+def fetch_ticker(ticker_name,timerange):
+    print()
+
+
+def get_data_for_ticker_in_range(ticker_name,from_date,to_date, data_type):
+    print()
+
+
+def get_profit_for_ticker_in_range(ticker_name,from_date,to_date,
+accumulated=False):
+    print()
+
+
+def get_p2v_for_ticker_in_range(ticker_name,from_date,to_date):
+    print()
+
+
